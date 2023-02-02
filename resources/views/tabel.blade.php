b82e17d9d757071b1aa362cfc7f6973e6d928317
qilal <aaqilalhafaafi@gmail.com>
2023-02-02 16:15:12 +0700

    tampilan user + tabel pesanan3

--- resources/views/tabel.blade.php
+++ resources/views/tabel.blade.php
@@ -5,20 +5,7 @@
         <div class="card shadow mb-4">
             <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
                 <h6 class="m-0 font-weight-bold text-primary">Nama Lapangan</h6>
-                <div class="dropdown no-arrow">
-                    <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown"
-                        aria-haspopup="true" aria-expanded="false">
-                        <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in"
-                        aria-labelledby="dropdownMenuLink">
-                        <div class="dropdown-header">Dropdown Header:</div>
-                        <a class="dropdown-item" href="#">Action</a>
-                        <a class="dropdown-item" href="#">Another action</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#">Something else here</a>
-                    </div>
-                </div>
+
             </div>
             <table class="table">
                 <thead class="thead-dark">
@@ -33,28 +20,38 @@
                     @foreach ($hours as $hour)
                         <tr>
                             <th scope="row">{{ $hour->jam }}</th>
-                            <td> <label class="btn btn-user-daftar" for="minggu"><input class="btn-check text-nowrap"
-                                        id="minggu" name="hours[]" type="checkbox" value="" autocomplete="off">
-                                    45000</td>
-                            <td> <label class="btn btn-user-daftar" for="minggu"><input class="btn-check text-nowrap"
-                                        id="minggu" name="hours[]" type="checkbox" value="" autocomplete="off">
-                                    45000</td>
-                            <td> <label class="btn btn-user-daftar" for="minggu"><input class="btn-check text-nowrap"
-                                        id="minggu" name="hours[]" type="checkbox" value="" autocomplete="off">
-                                    45000</td>
-                            <td> <label class="btn btn-user-daftar" for="minggu"><input class="btn-check text-nowrap"
-                                        id="minggu" name="hours[]" type="checkbox" value="" autocomplete="off">
-                                    45000</td>
-                            <td><label class="btn btn-success" for="jumat"><input class="btn-check text-nowrap"
-                                        id="jumat" name="hours[]" type="checkbox" value=""checked disabled>
-                                    45000</td>
-                            <td><label class="btn btn-danger" for="sabtu"><input class="btn-check text-nowrap"
-                                        id="sabtu" name="hours[]" type="checkbox" value=""disabled>
-                                    60000</td>
+                            <td> <input class="btn-check text-nowrap" id="($hours as $hour)" name="hours[]" type="checkbox"
+                                    value="" autocomplete="off"> <label class="btn btn-user-daftar"
+                                    for="($hours as $hour)">
+                                    45000</label></td>
+                            <td> <input class="btn-check text-nowrap" id="($hours as $hour)" name="hours[]" type="checkbox"
+                                    value="" autocomplete="off"> <label class="btn btn-user-daftar"
+                                    for="($hours as $hour)">
+                                    45000</label></td>
+                            <td> <input class="btn-check text-nowrap" id="($hours as $hour)" name="hours[]" type="checkbox"
+                                    value="" autocomplete="off"> <label class="btn btn-user-daftar"
+                                    for="($hours as $hour)">
+                                    45000</label></td>
+                            <td> <input class="btn-check text-nowrap" id="($hours as $hour)" name="hours[]" type="checkbox"
+                                    value="" autocomplete="off"> <label class="btn btn-user-daftar"
+                                    for="($hours as $hour)">
+                                    45000</label></td>
+                            <td><input class="btn-check text-nowrap" id="($hours as $hour)" name="hours[]" type="checkbox"
+                                    value="" autocomplete="off"> <label class="btn btn-user-daftar"
+                                    for="($hours as $hour)">
+                                    45000</label></td>
                             <td>
-                                <label class="btn btn-user-daftar" for="minggu"><input class="btn-check text-nowrap"
-                                        id="minggu" name="hours[]" type="checkbox" value="" autocomplete="off">
+                                <input class="btn-check text-nowrap" id="($hours as $hour)" name="hours[]" type="checkbox"
+                                    value=""disabled>
+                                <label class="btn btn-danger" for="sabtu">60000</label>
+                            </td>
+
+                            <td>
+                                <input class="btn-check text-nowrap" id="minggu" name="hours[]" type="checkbox"
+                                    value="" checked autocomplete="off"> <label class="btn btn-user-daftar"
+                                    for="minggu">
                                     80000</label>
+                            </td>
                             </td>
                         </tr>
                     @endforeach
@@ -73,4 +70,4 @@
             </div>
         </div>
     </div>
-    @endsection
+@endsection
